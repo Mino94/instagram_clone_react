75f402950ddb997048ac74d9dcc0cd024fdb80bb
DESKTOP-1Q3I6GN\BIT <mh.kim368@gmail.com>
2022-05-31 14:09:02 +0900

    join 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import Login from "./components/Login/Login";
 import BootstrapLogin from "./components/Login/BootstrapLogin";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./components/Main";
+import Page404 from "./components/Page404";
+
 function App() {
   return (
     <div className="App">
-      <BootstrapLogin></BootstrapLogin>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Main></Main>}></Route>
+          <Route
+            index
+            path="/login"
+            element={<BootstrapLogin></BootstrapLogin>}
+          ></Route>
+          <Route index path="/*" element={<Page404></Page404>}></Route>
+          <Route index path="/join" element={<Join></Join>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Login/BootstrapLogin.js
+++ src/components/Login/BootstrapLogin.js
@@ -1,27 +1,74 @@
+import { useState } from "react";
+import { Button, Col, Container, Form, Input, Row, Alert } from "reactstrap";
+import { Users } from "./Users";
 import "./Login.css";
-
-import { Col, Row, Container, Button, Input, Form } from "reactstrap";
+import { useNavigate } from "react-router";
+import AuthRouter from "../AuthRouter";
 
 const BootstrapLogin = () => {
+  const [isFail, setIsFail] = useState(false);
+
+  const [user, setUser] = useState({
+    id: "",
+    password: "",
+  });
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const navigate = useNavigate();
+
+  const onSubmitLogin = (e) => {
+    e.preventDefault();
+    const findUser = Users.find(
+      (data) => data.userId === user.id && data.password === user.password
+    );
+    if (findUser) {
+      localStorage.setItem("id", findUser.id);
+      navigate("/");
+    } else {
+      setIsFail(true);
+      setTimeout(() => closeAlert(), 3000);
+    }
+  };
+
+  const closeAlert = () => {
+    setIsFail(false);
+  };
+
   return (
     <div className="LoginPage">
       <Container className="bg-light border">
-        <Row style={{ rowGap: "item", padding: "3em" }}>
+        <Row style={{ rowGap: "1em", padding: "3em" }}>
           <Col xl={12}>
             <img
               src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
               alt="Logo"
             ></img>
           </Col>
+
           <Col xl={12}>
-            <Form className="LoginForm">
-              <Input type={"text"} placeholder="ID" name="id"></Input>
+            <Form onSubmit={onSubmitLogin} className="LoginForm">
+              {isFail ? (
+                <Alert color="warning" toggle={() => closeAlert}>
+                  아이디 또는 비밀번호가 틀렸습니다.
+                </Alert>
+              ) : null}
               <Input
-                type={"password"}
-                placeholder="password"
-                name="password"
+                type="text"
+                placeholder="Id"
+                name="id"
+                onChange={(e) => onChangeHandler(e)}
               ></Input>
-              <Button type={"submit"} color="primary">
+              <Input
+                type="password"
+                placeholder="Password"
+                name="password"
+                onChange={(e) => onChangeHandler(e)}
+              ></Input>
+              <Button type={"submit"} color="primary" block>
                 로그인
               </Button>
             </Form>
@@ -30,12 +77,14 @@ const BootstrapLogin = () => {
       </Container>
 
       <Container className="bg-light border">
-        <Row style={{ rowGap: "item", padding: "3em" }}>
+        <Row style={{ padding: "1em", textAlign: "center" }}>
           <p>
-            계정이 없으신가요? <a href="#">가입하기</a>
+            계정이 없으신가요? <a href="g">가입하기</a>
           </p>
         </Row>
       </Container>
+
+      <AuthRouter></AuthRouter>
     </div>
   );
 };
