e024d36cc8c5ec8b9a99acfaba190be064b13772
DESKTOP-1Q3I6GN\BIT <mh.kim368@gmail.com>
2022-05-31 11:27:27 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./components/Login/Login";
+import BootstrapLogin from "./components/Login/BootstrapLogin";
+function App() {
+  return (
+    <div className="App">
+      <BootstrapLogin></BootstrapLogin>
+    </div>
+  );
+}
+
+export default App;
